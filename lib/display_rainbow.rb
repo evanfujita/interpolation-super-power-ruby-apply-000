df380d22eeb918d9a326cc560211fa9996150693
magical-tutorial-4913 <evanfujita@gmail.com>
2020-09-30 17:20:26 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,3 +8,5 @@ def display_rainbow(colors)
   puts "#{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}"
 end
   
+  
+display_rainbow(red, orange, yellow, green, blue, indigo, violet)
