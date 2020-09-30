4e48e38a9206e96b753db20db9200c46d69726bb
magical-tutorial-4913 <evanfujita@gmail.com>
2020-09-30 17:18:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,12 +5,6 @@ colors = [red = "R: red", orange = "O: orange", yellow = "Y: yellow", green = "G
 
 def display_rainbow(colors)
   puts "The colors of the rainbow are:"
-  puts "#{colors[0]}"
-  puts "#{colors[1]}"
-  puts "#{colors[2]}"
-  puts "#{colors[3]}"
-  puts "#{colors[4]}"
-  puts "#{colors[5]}"
-  puts "#{colors[6]}"
+  puts "#{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}"
 end
   
